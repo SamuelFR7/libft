e92bd0c2b181d79c46a26c461d9cfea770586ca4
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-09-15 20:25:51 -0300

    feat: ft_memset implemented

---
+++ include/libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+void        *ft_memset(void *s, int c, size_t n);
+size_t      *ft_strlen(const char *str);
+int         ft_isdigit(const int c);
+int         ft_tolower(int c);
+int         ft_toupper(int c);
+
+
+#endif
---
+++ src/ft_memset.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void *ft_memset(void *s, int c, size_t n) {
+    unsigned int i;
+    char *cptr;
+
+    i = 0;
+    cptr = (char *) s;
+    while (i < n) {
+        cptr[i] = c;
+        i++;
+    }
+
+    return ((void *) cptr);
+}
